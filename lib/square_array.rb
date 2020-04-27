765c20cf380496c046e0ef01709a0a32195a2a48
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-27 16:17:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,3 @@
 def square_array(array)
-  counter = 0
-  while counter < array.length do
-    new_arr = []
-    new_num = array[counter] * array[counter]
-    new_arr.push(new_num)
-    counter += 1;
-  end
-  return new_arr
+ a.each {|x| print x, " -- " }
 end
